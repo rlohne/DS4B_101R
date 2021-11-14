b738b57b369b640ccc2a95305db7a8ac320d3ab0
Robert Lohne <55624014+rlohne@users.noreply.github.com>
2021-11-14 22:12:47 +0100

    27 % done

--- 02_data_wrangling/03_time_series.R
+++ 02_data_wrangling/03_time_series.R
@@ -21,12 +21,17 @@ order_date_tbl %>%
   pull() %>% 
   class()
 
+# 1.2 Date Classes
+
 order_date_tbl %>% 
   mutate(
-    order_date_chr = as.character(order_date)
-  )
+    order_date_chr = as.character(order_date) 
+  ) %>% 
+  mutate(order_date_chr2 = order_date_chr %>% str_c(" 00:00:00")) %>% 
+  mutate(order_date_date = order_date_chr %>% ymd()) %>% 
+  mutate(order_date_dttm = order_date_chr2 %>% ymd_hms())
 
-# 1.2 Date Classes
+
 
 
 
@@ -37,31 +42,85 @@ order_date_tbl %>%
 
 # Conversion
 
+"06/01/18" %>% 
+  mdy()
+
+"06/01/18 12:30:15" %>% 
+  mdy_hms()
+
+
+"January 1, 1985" %>% mdy()
 
 
 # Extractor
 
+"2011-01-01" %>%  ymd() %>% year()
+"2011-01-01" %>%  ymd() %>% month(label = TRUE, abbr = FALSE)
+"2011-01-01" %>%  ymd() %>% wday(label = TRUE, abbr = FALSE) 
+"2011-01-01" %>%  ymd() %>% day()
+"2011-01-01" %>%  ymd() %>% isoweek()
 
 
 # Helpers
+now() # datetime 
+today() # date
 
 
 
 # Periods & Durations - Add/subract time to/from a date
+today() + days(12) # period
+today() + ddays(12) # duration 
 
-
+today() + years(8)
+today() + dyears(8)
 
 # Intervals - Calculate time-based distance 
 
+i <- interval(today(), today() + ddays(12)) 
 
+i / ddays(1)
+
+i / dminutes(1)
+
+order_date_tbl %>% 
+  mutate(today = today()) %>% 
+  mutate(diff_days = interval(order_date, today) / ddays(1))
 
 # 2.0 Time-Based Data Grouping ----
 
+bike_sales_y_tbl <- bike_orderlines_tbl %>% 
+  select(order_date, total_price) %>% 
+  
+  # lubridate
+  mutate(order_date = ymd(order_date)) %>% 
+  mutate(year = year(order_date)) %>% 
+  
+  # grouping and summarise
+  group_by(year) %>% 
+  summarise(
+    sales = sum(total_price)
+  ) %>% 
+  ungroup()
 
+bike_sales_y_tbl
 
+bike_sales_ym_tbl <- bike_orderlines_tbl %>% 
+  select(order_date, total_price) %>% 
+  
+  # lubridate
+  mutate(order_date = ymd(order_date)) %>% 
+  mutate(
+    year  = year(order_date),
+    month = month(order_date, label = TRUE, abbr = TRUE)) %>% 
+  
+  # grouping and summarise
+  group_by(year, month) %>% 
+  summarise(
+    sales = sum(total_price)
+  ) %>% 
+  ungroup()
 
-
-
+bike_sales_ym_tbl
 
 # 3.0 Measuring Change ----
 
