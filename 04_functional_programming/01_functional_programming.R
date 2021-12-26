477408a0c0d8c6e840a086850b9eecf4fe70dc70
Robert Lohne <55624014+rlohne@users.noreply.github.com>
2021-12-26 20:20:11 +0100

    50 %

--- 04_functional_programming/01_functional_programming.R
+++ 04_functional_programming/01_functional_programming.R
@@ -50,10 +50,35 @@ mean_remove_na <- function(x, na.rm = TRUE, ...) {
 # Calculating a 3 month rolling average  for category_1 & category_2 
 # with dates aligned at last day of the month
 
+rolling_avg_3 <- bike_orderlines_tbl %>% 
+    select(order_date, category_1, category_2, total_price) %>% 
+    
+    mutate(order_date = ymd(order_date)) %>% 
+    mutate(month_end = ceiling_date(order_date, unit = "month") - period(1, units = "day")) %>% 
+    group_by(category_1, category_2, month_end) %>% 
+    summarise(
+        total_price = sum(total_price)
+    ) %>% 
+    
+    mutate(rolling_avg_3 =  rollmean(total_price, k = 3, na.pad = TRUE, align = "right")) %>% 
+    ungroup() %>% 
+    
+    mutate(category_2 = as_factor(category_2) %>% fct_reorder2(month_end, total_price))
+    
+rolling_avg_3 %>% 
+    ggplot(aes(
+        x = month_end,
+        y = total_price,
+        color = category_2
+    )) + 
+    geom_point() + 
+    geom_line(aes(y = rolling_avg_3), color = "blue", size = 1) + 
+    facet_wrap(~ category_2, scales = "free_y") + 
+    theme_tq() + 
+    scale_color_tq()
 
 
-
-# 2.1 Vector Functions ----
+    # 2.1 Vector Functions ----
 
 
 # 2.2 Data Functions ----
